4700ab24c05b941d3df2ff0d33360fe187d1eda7
Vishal Kumawat <161027477+VishalKumawat19@users.noreply.github.com>
2024-09-10 00:22:02 +0530

    Update cors options

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const authRoutes = require('./routes/authRoutes');
 const cors = require('cors')
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://auth-auto-logout-ui.vercel.app',
     credentials: true
   }
 app.use(cors(corsOptions))
